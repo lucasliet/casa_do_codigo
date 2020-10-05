6c582e2c762764b7a82087751bcc27e0c0382143
lsoliveira <lsoliveira@magnasistemas.com.br>
2020-10-05 12:33:34 -0300

    add dao and transactional to bean

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.LivroDAO;
 import br.com.casadocodigo.loja.models.Livro;
 
 @Named
@@ -11,7 +14,12 @@ public class AdminLivrosBean {
 
 	private Livro livro = new Livro();
 
+	@Inject
+	private LivroDAO dao;
+	
+	@Transactional
 	public void salvar() {
+		dao.salvar(livro);
 		System.out.println("Livro cadastrado: " + livro);
 
 	}
