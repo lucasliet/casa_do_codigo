ada73712da373c0e89fdc1d2acf5392ebb01a930
lucasliet <lucasouliveira@gmail.com>
2020-10-05 12:10:58 -0300

    init

---
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDAO.java
@@ -0,0 +1,17 @@
+package br.com.casadocodigo.loja.daos;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.casadocodigo.loja.models.Livro;
+
+public class LivroDAO {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void salvar(Livro livro) {
+		entityManager.persist(livro);
+	}
+
+}
