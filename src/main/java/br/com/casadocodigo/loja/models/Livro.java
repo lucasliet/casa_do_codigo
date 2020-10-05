4d78659f02da4985a3d3d40e851d73bf6e333076
lucasliet <lucasouliveira@gmail.com>
2020-10-05 17:55:00 -0300

    Bean Validation

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -10,6 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Livro {
@@ -17,14 +24,20 @@ public class Livro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	@NotBlank
 	private String titulo;
 	@Lob // Coluna com grande espaço de dados
+	@Length(min=10)
+	@NotBlank
 	private String descricao;
+	@DecimalMin("20")
 	private BigDecimal preco;
+	@Min(50)
 	private Integer numeroPaginas;
 
 	@ManyToMany
+	@Size(min=1)
+	@NotNull
 	private List<Autor> autores = new ArrayList<>();
 
 	public List<Autor> getAutores() {
